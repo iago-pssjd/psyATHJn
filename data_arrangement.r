82259342fdb3dd60ad2a1825ab333e4b9ba08846
iago-pssjd <iago.gine@sjd.es>
2022-08-23 09:38:56 +0200

    some w10-RStudio indentations; education imputation changes (and added MHAS); build and save final dataset; other checks

--- data_arrangement.r
+++ data_arrangement.r
@@ -3,16 +3,16 @@
 ### data loading
 
 if(Sys.info()["sysname"] == "Linux"){
-	data_add <- paste0("/media/",
-			   system("whoami", intern = TRUE),
-			   "/",
-			   system(paste0("ls /media/",system("whoami", intern = TRUE)), intern = TRUE),
-			   "/PSSJD/athlos/data/")
-	data_add <- data_add[file.exists(data_add)]
+  data_add <- paste0("/media/",
+                     system("whoami", intern = TRUE),
+                     "/",
+                     system(paste0("ls /media/",system("whoami", intern = TRUE)), intern = TRUE),
+                     "/PSSJD/athlos/data/")
+  data_add <- data_add[file.exists(data_add)]
 } else if(Sys.info()["sysname"] == "Windows"){
-	data_add <- paste0(grep("^[A-Z]:$", sub(":(.*)", ":",shell("wmic logicaldisk get name", intern = TRUE)), value = TRUE), "/PSSJD/athlos/data")
-	data_add <- data_add[file.exists(data_add)]
-	data_add <- paste0(data_add, "/")
+  data_add <- paste0(grep("^[A-Z]:$", sub(":(.*)", ":",shell("wmic logicaldisk get name", intern = TRUE)), value = TRUE), "/PSSJD/athlos/data")
+  data_add <- data_add[file.exists(data_add)]
+  data_add <- paste0(data_add, "/")
 }
 
 load(paste0(data_add, "athlos_v2.1.1.rdata"))
@@ -31,12 +31,19 @@ setorder(athlos, study, cohort, country, athlos_id, wave)
 # For CHARLS, are generally 45+ (1stQ =50/51), so it is also imputed
 athlos[, education := fcase(study == "10/66" & wave == 2 & !is.na(yintw), shift(education, n = 1L, type = "lag"), 
                             study == "ALSA" & wave == 2 & is.na(education), shift(education, n = 1L, type = "lag"),
-                            study == "ALSA" & wave == 11 & !is.na(yintw), shift(education, n = 9L, type = "lag"),
                             study == "CHARLS" & wave == 2 & is.na(education) & !is.na(yintw), shift(education, n = 1L, type = "lag"),
-			    study == "JSTAR" & wave == 2 & !is.na(yintw), shift(education, n = 1L, type = "lag"),
-			    study == "JSTAR" & wave == 3 & !is.na(yintw), shift(education, n = 2L, type = "lag"),
+                            study == "JSTAR" & wave == 2 & !is.na(yintw), shift(education, n = 1L, type = "lag"),
+                            study == "MHAS" & wave == 2 & is.na(education), shift(education, n = 1L, type = "lag"),
                             rep(TRUE, .N),  education), 
-       by = .(athlos_id2)]
+       by = .(athlos_id2)
+       ][, education := fcase(study == "ALSA" & wave == 11 & !is.na(yintw), shift(education, n = 9L, type = "lag"),
+                              study == "JSTAR" & wave == 3 & !is.na(yintw), shift(education, n = 1L, type = "lag"),
+                              study == "MHAS" & wave == 3 & !is.na(yintw) & is.na(education), shift(education, n = 1L, type = "lag"),
+                              rep(TRUE, .N),  education), 
+         by = .(athlos_id2)
+         ][, education := fcase(study == "MHAS" & wave == 3 & !is.na(yintw) & is.na(education), shift(education, n = 2L, type = "lag"),
+                                rep(TRUE, .N),  education), 
+           by = .(athlos_id2)]
 
 # athlos[, .(athlos_id, athlos_id2, study, cohort, wave, country, age, sex, marital_status, education, employed, retired, wealth, healthstatus, resid_place, confidant, spouse, cont_fr, cont_rel, depression, anxiety_symp, loneliness)
 #        ][, lapply(.SD, \(.x) sum(is.na(.x)))]
@@ -50,16 +57,18 @@ athlos[, .(athlos_id2, study, cohort, wave, country, yintw,
            age, sex, marital_status, education, employed, healthstatus, resid_place, 
            confidant, loneliness,
            depression, anxiety_symp, suicidal_ideation_12m, suicidal_ideation_lm)
-       ][, lapply(.SD, \(.x) sum(is.na(.x)))]
+][, lapply(.SD, \(.x) sum(is.na(.x)))]
 
 
-athlos[study == "TILDA", .(athlos_id2, study, cohort, wave, country, yintw, 
-           age, sex, marital_status, education, employed, healthstatus, resid_place, 
-           confidant, loneliness, 
-           depression, anxiety_symp, suicidal_ideation_12m, suicidal_ideation_lm)
-       ][, lapply(.SD, \(.x) sum(!is.na(.x))), by = .(study, cohort, wave)] |> 
+psathlos[, .(athlos_id2, study, cohort, wave, country, yintw, 
+                                              age, sex, marital_status, education, healthstatus, 
+                                              loneliness, 
+                                              depression, anxiety_symp)
+         ][, lapply(.SD, \(.x) sum(is.na(.x))), by = .(study, cohort, wave)] |> 
   t() |> 
   View()
+
+athlos[study=="MHAS" & wave == 3 & !is.na(yintw) & is.na(education) & (athlos_id %in% athlos[study == "MHAS" & wave == 1 & !is.na(yintw) & !is.na(education)]$athlos_id)]$age |> quantile(probs = seq(0,1,0.05), na.rm = TRUE)
 
 options(max.print = 999999)
 athlos[study=="SHARE", .(.N), by = .(study, cohort, wave, yintw)] |> print(nrows = 999)
@@ -89,3 +98,33 @@ athlos[order(study, cohort, country, wave, yintw), .(.N), by = .(study, cohort, 
 # SAGE study is outside years range 2010-2015
 # SHARE wave 5
 # TILDA wave 1
+
+psathlos <- athlos[((study == "10/66" & wave == 2) | 
+                      (study == "ALSA" & wave == 11) | 
+                      (study == "CHARLS" & wave == 2) | 
+                      (study == "COURAGE" & wave == 1 & cohort != "Finland") |
+                      (study == "ELSA" & wave == 6) |
+                      (study == "HRS" & wave == 10) |
+                      (study == "Health2000-2011" & wave == 2) |
+                      (study == "JSTAR" & yintw == 2011) |
+                      (study == "KLOSA" & wave == 3) |
+                      (study == "LASI") |
+                      (study == "MHAS" & wave == 3) |
+                      (study == "SHARE" & wave == 5) |
+                      (study == "TILDA" & wave == 1)) & !is.na(yintw), .(athlos_id2, study, cohort, wave, country, yintw, 
+                                                                         age, sex, marital_status, education, employed, healthstatus, resid_place, 
+                                                                         confidant, loneliness, 
+                                                                         depression, anxiety_symp, suicidal_ideation_12m, suicidal_ideation_lm)]
+
+# psathlos <- na.omit(psathlos, cols = c("age", "sex", "loneliness"))
+ 
+# library(dplyr)
+# library(dtplyr)
+# psathlos_dt <- lazy_dt(psathlos)
+# psathlos_dt |> 
+#   filter(!if_any(c(age, sex, loneliness), is.na)) |> 
+#   as.data.table() |> 
+#   dim()
+
+library(haven)
+write_dta(psathlos, path = paste0(data_add, "psathlos.rdata"))
